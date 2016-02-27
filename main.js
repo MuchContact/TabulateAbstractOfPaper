5237f528179e1a22d524d973a277efa1f689f157
hjliu <hjliu@thoughtworks.com>
2016-02-27 23:37:44 +0800

    extract main.js

--- main.js
+++ main.js
@@ -1,4 +1,44 @@
 ﻿// main.js
 window.onload = function(){
-	var client = new ZeroClipboard( document.getElementById("zeroCopy") );	
+	var client = new ZeroClipboard( document.getElementById("zeroCopy") );
+}
+//delete all the blank characters.
+String.prototype.mtrim = function(){
+	return this.replace(/\s/g,"");
+}
+//格式化
+function format(){
+	var str=getSourceContent();
+	if(str==""){
+		return ;
+	}
+	var result = tabularSourceString(str);
+	setResultContent(result);
+}
+
+function getSourceContent(){
+	return document.getElementById("abstract").value.mtrim();
+}
+
+function tabularSourceString(source){
+	var cols = parseInt(document.getElementById("abstractionSource").value);
+	var textIndent = document.getElementById("textIndent").checked;
+	if(textIndent){
+		//append the source string with additional two blank characters at head;
+		source = "  "+source;
+	}
+	var result = "";
+	for(var i=0;i<source.length;i++){
+		result += source.charAt(i);
+		if((i+1)%cols==0){
+			result += "\r\n";
+		}else{
+			result += "\t";
+		}
+	}
+	return result;
+}
+
+function setResultContent(content){
+	document.getElementById("result").value = content;
 }
